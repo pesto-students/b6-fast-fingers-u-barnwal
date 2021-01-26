e2c947cd901e2c888b5a002f0e6858e6438f8df3
Utsav Barnwal <u.barnwal36@gmail.com>
2021-01-26 23:45:04 +0530

    wip: design landing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./css/App.css";
 
+// + Image
+import imgLogo from "./images/logo.svg";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Fast Fingers</h1>
-    </div>
+    <main>
+      <div className="landing">
+        <img src={imgLogo} alt="" />
+
+        <h1>Fast Fingers</h1>
+
+        <div className="labeled-hr">
+          <div></div>
+          <span>The ultimate typing game</span>
+          <div></div>
+        </div>
+
+        <div className="wrap-inputs">
+          <input type="text" placeholder="Type your name..." />
+        </div>
+      </div>
+
+      <div className="game"></div>
+    </main>
   );
 }
 
